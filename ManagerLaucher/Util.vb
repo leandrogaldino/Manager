6071d26ac01f612360bb51e2a13d847baf3387a7
Leandro Correia Galdino <62770546+leandrogaldino@users.noreply.github.com>
2025-10-06 09:39:33 -0300

    update

--- ManagerLaucher/Util.vb
+++ ManagerLaucher/Util.vb
@@ -2,43 +2,54 @@
 
 Public Class Util
     Public Shared Function HasUpdate(DeployDirectory As String, ManagerDirectory As String) As Boolean
-        Dim CurrentVersion As String = GetCurrentVersion(ManagerDirectory)
-        Dim DeployVersion As String = GetDeployVersion(DeployDirectory)
-        If String.IsNullOrEmpty(CurrentVersion) And String.IsNullOrEmpty(DeployVersion) Then
+        Dim CurrentVersionStr As String = GetCurrentVersion(ManagerDirectory)
+        Dim DeployVersionStr As String = GetDeployVersion(DeployDirectory)
+
+        ' Se não houver nenhuma versão, não há atualização
+        If String.IsNullOrEmpty(CurrentVersionStr) AndAlso String.IsNullOrEmpty(DeployVersionStr) Then
             Return False
-        ElseIf Not String.IsNullOrEmpty(CurrentVersion) And String.IsNullOrEmpty(DeployVersion) Then
-            Return False
-        ElseIf String.IsNullOrEmpty(CurrentVersion) And Not String.IsNullOrEmpty(DeployVersion) Then
-            Return True
-        Else
-            Return CurrentVersion.CompareTo(DeployVersion) < 0
         End If
+
+        ' Se o deploy tem versão mas o atual não, então há update
+        If String.IsNullOrEmpty(CurrentVersionStr) AndAlso Not String.IsNullOrEmpty(DeployVersionStr) Then
+            Return True
+        End If
+
+        ' Se o atual tem versão mas o deploy não, então não há update
+        If Not String.IsNullOrEmpty(CurrentVersionStr) AndAlso String.IsNullOrEmpty(DeployVersionStr) Then
+            Return False
+        End If
+
+        ' Tenta converter as versões para objeto Version e comparar numericamente
+        Try
+            Dim currentVer As New Version(CurrentVersionStr)
+            Dim deployVer As New Version(DeployVersionStr)
+            Return deployVer > currentVer
+        Catch ex As Exception
+            ' Se algo der errado na conversão, evita crash
+            Return False
+        End Try
     End Function
+
 
     Private Shared Function GetCurrentVersion(ManagerDirectory As String) As String
         Dim ManagerFile As String = Path.Combine(ManagerDirectory, "App", "Manager.exe")
-        Dim Assembly As Reflection.Assembly
-        Dim VersionInfo As FileVersionInfo
-        Dim Version As String
+
         If File.Exists(ManagerFile) Then
-            Assembly = Reflection.Assembly.GetExecutingAssembly()
-            VersionInfo = FileVersionInfo.GetVersionInfo(ManagerFile)
-            Version = $"{VersionInfo.FileMajorPart}.{VersionInfo.FileMinorPart}"
-            Return Version
+            Dim VersionInfo As FileVersionInfo = FileVersionInfo.GetVersionInfo(ManagerFile)
+            Return $"{VersionInfo.FileMajorPart}.{VersionInfo.FileMinorPart}.{VersionInfo.FileBuildPart}"
         Else
             Return String.Empty
         End If
     End Function
+
+
     Private Shared Function GetDeployVersion(DeployDirectory As String) As String
         Dim ManagerFile As String = Path.Combine(DeployDirectory, "Manager.exe")
-        Dim Assembly As Reflection.Assembly
-        Dim VersionInfo As FileVersionInfo
-        Dim Version As String
+
         If File.Exists(ManagerFile) Then
-            Assembly = Reflection.Assembly.GetExecutingAssembly()
-            VersionInfo = FileVersionInfo.GetVersionInfo(ManagerFile)
-            Version = $"{VersionInfo.FileMajorPart}.{VersionInfo.FileMinorPart}"
-            Return Version
+            Dim VersionInfo As FileVersionInfo = FileVersionInfo.GetVersionInfo(ManagerFile)
+            Return $"{VersionInfo.FileMajorPart}.{VersionInfo.FileMinorPart}.{VersionInfo.FileBuildPart}"
         Else
             Return String.Empty
         End If

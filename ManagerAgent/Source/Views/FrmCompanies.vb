3f83efe39d6b5c8794b2332f702893f26db0efff
Leandro Correia Galdino <leandrocgaldino@gmail.com>
2025-12-27 10:03:31 -0300

    update

--- ManagerAgent/Source/Views/FrmCompanies.vb
+++ ManagerAgent/Source/Views/FrmCompanies.vb
@@ -2,12 +2,16 @@
 
 
     Private Sub OpenCompany(Model As CompanyModel)
-        MsgBox(Model.Name)
+        Using Form As New FrmCompany(Model)
+            Form.ShowDialog()
+        End Using
     End Sub
 
     Private Sub FrmCompanies_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-        Dim ct As New UcCompanyTile(New CompanyModel() With {
+        Dim ct As UcCompanyTile
+
+        ct = New UcCompanyTile(New CompanyModel() With {
             .Name = "Tech Solutions",
             .Document = "34.567.890/0001-12",
             .Address = "Rio de Janeiro - RJ",
@@ -17,54 +21,75 @@
         FlpPrivilege.Controls.Add(ct)
 
 
-        FlpPrivilege.Controls.Add(New UcCompanyTile(New CompanyModel() With {
+        ct = New UcCompanyTile(New CompanyModel() With {
             .Name = "Reicol Comércio",
             .Document = "12.764.271/0001-00",
             .Address = "Goiânia - GO",
             .LogoFileName = "reicol.png"
-        }))
+        })
+        ct.OnClickAction = AddressOf OpenCompany
+        FlpPrivilege.Controls.Add(ct)
 
-        FlpPrivilege.Controls.Add(New UcCompanyTile(New CompanyModel() With {
+
+        ct = New UcCompanyTile(New CompanyModel() With {
             .Name = "Nexor Sistemas Ltda",
             .Document = "45.892.113/0001-42",
             .Address = "São Paulo - SP",
             .LogoFileName = "nexor.png"
-        }))
+        })
+        ct.OnClickAction = AddressOf OpenCompany
+        FlpPrivilege.Controls.Add(ct)
 
-        FlpPrivilege.Controls.Add(New UcCompanyTile(New CompanyModel() With {
+
+        ct = New UcCompanyTile(New CompanyModel() With {
             .Name = "Alfa Distribuidora",
             .Document = "09.331.764/0001-85",
             .Address = "Campinas - SP",
             .LogoFileName = "alfa.png"
-        }))
+        })
+        ct.OnClickAction = AddressOf OpenCompany
+        FlpPrivilege.Controls.Add(ct)
 
-        FlpPrivilege.Controls.Add(New UcCompanyTile(New CompanyModel() With {
+
+        ct = New UcCompanyTile(New CompanyModel() With {
             .Name = "Horizonte Tecnologia",
             .Document = "33.508.921/0001-10",
             .Address = "Belo Horizonte - MG",
             .LogoFileName = "horizonte.png"
-        }))
+        })
+        ct.OnClickAction = AddressOf OpenCompany
+        FlpPrivilege.Controls.Add(ct)
 
-        FlpPrivilege.Controls.Add(New UcCompanyTile(New CompanyModel() With {
+
+        ct = New UcCompanyTile(New CompanyModel() With {
             .Name = "Prime Solutions",
             .Document = "61.774.205/0001-67",
             .Address = "Curitiba - PR",
             .LogoFileName = "prime.png"
-        }))
+        })
+        ct.OnClickAction = AddressOf OpenCompany
+        FlpPrivilege.Controls.Add(ct)
 
-        FlpPrivilege.Controls.Add(New UcCompanyTile(New CompanyModel() With {
+
+        ct = New UcCompanyTile(New CompanyModel() With {
             .Name = "Solaris Indústria",
             .Document = "28.119.450/0001-03",
             .Address = "Joinville - SC",
             .LogoFileName = "solaris.png"
-        }))
+        })
+        ct.OnClickAction = AddressOf OpenCompany
+        FlpPrivilege.Controls.Add(ct)
 
-        FlpPrivilege.Controls.Add(New UcCompanyTile(New CompanyModel() With {
+
+        ct = New UcCompanyTile(New CompanyModel() With {
             .Name = "Vértice Comércio e Serviços",
             .Document = "74.902.388/0001-91",
             .Address = "Porto Alegre - RS",
             .LogoFileName = "vertice.png"
-        }))
+        })
+        ct.OnClickAction = AddressOf OpenCompany
+        FlpPrivilege.Controls.Add(ct)
+
 
     End Sub
 
--- ManagerAgent/Source/Views/Settings/FrmCompany.vb
+++ ManagerAgent/Source/Views/Settings/FrmCompany.vb
@@ -1,3 +1,10 @@
 ﻿Public Class FrmCompany
+    Public Sub New(Company As CompanyModel)
 
+
+        InitializeComponent()
+
+        TxtName.Text = Company.Name
+
+    End Sub
 End Class
